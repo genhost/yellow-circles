6988a323bca8ee4b8e0184417ad7c092d59c0d8d
Mark Meliksetyan <markmelix@gmail.com>
2022-11-21 17:54:06 +0300

    Implement random circle color

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
-import random
 
 from PyQt5 import uic
-from PyQt5.QtCore import QRectF
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+from ui import Ui_MainWindow
 
 
-class Circles(QMainWindow):
+class Circles(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.do_draw = False
         self.draw_button.clicked.connect(self.draw_true)
 
@@ -22,9 +22,9 @@ class Circles(QMainWindow):
         if self.do_draw:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
 
-            d = random.randint(20, 200)
+            d = randint(20, 200)
 
             qp.drawEllipse(300 - d // 2, 100 - d // 2, d, d)
             qp.end()
